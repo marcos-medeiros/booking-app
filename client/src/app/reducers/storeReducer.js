183f798e830cde6a4d0beefc6d06de1f04f23781
marcos medeiros <mvsmdrs@protonmail.com>
2020-04-01 20:35:18 -0300

    Create storeReducer

---
+++ client/src/app/reducers/storeReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+import appointments from './appointments';
+import doctor from './doctor';
+import filter from './filter';
+import main from './main';
+
+const storeReducer = combineReducers({
+    appointments,
+    doctor,
+    filter,
+    main,
+})
+
+export default storeReducer;
