b8095bacbfc63d4c475d9ba4b3b2ffc1d02ab6a6
marcos medeiros <mvsmdrs@protonmail.com>
2020-04-01 19:35:05 -0300

    Fix import statement linter error

--- client/src/app/actions/actions.js
+++ client/src/app/actions/actions.js
@@ -1,33 +1,35 @@
-import { ActionTypes } from './types';
+import ActionTypes from './types';
 
-const changeDoctorFilter = (filter) => ({
-    type: ActionTypes.CHANGE_DOCTOR_FILTER,
-    filter
+const changeDoctorFilter = filter => ({
+  type: ActionTypes.CHANGE_DOCTOR_FILTER,
+  filter,
 });
 
 const switchMain = () => ({
-    type: ActionTypes.SWITCH_MAIN
+  type: ActionTypes.SWITCH_MAIN,
 });
 
-const selectDoctor = (doctor) => ({
-    type: ActionTypes.SELECT_DOCTOR,
-    doctor
+const selectDoctor = doctor => ({
+  type: ActionTypes.SELECT_DOCTOR,
+  doctor,
 });
 
 const scheduleAppointment = (date, time, doctor, patient) => ({
-    type: ActionTypes.SCHEDULE_APPOINTMENT,
-    date,
-    time,
-    doctor,
-    patient
+  type: ActionTypes.SCHEDULE_APPOINTMENT,
+  date,
+  time,
+  doctor,
+  patient,
 });
 
 const cancelAppointment = (date, time, doctor, patient) => ({
-    type: ActionTypes.CANCEL_APPOINTMENT,
-    date,
-    time,
-    doctor,
-    patient
+  type: ActionTypes.CANCEL_APPOINTMENT,
+  date,
+  time,
+  doctor,
+  patient,
 });
 
-export { changeDoctorFilter, switchMain, selectDoctor, scheduleAppointment, cancelAppointment };
+export {
+  changeDoctorFilter, switchMain, selectDoctor, scheduleAppointment, cancelAppointment,
+};
