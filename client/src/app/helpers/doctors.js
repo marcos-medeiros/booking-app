5cab431a81c26951b6ddb9cb09561fb8aff7326e
marcos medeiros <mvsmdrs@protonmail.com>
2020-04-01 20:45:06 -0300

    Add doctors helper file

---
+++ client/src/app/helpers/doctors.js
@@ -0,0 +1,16 @@
+const doctors = [
+    {
+        name: 'John Silver',
+        speciality: 'Heart Surgery'
+    },
+    {
+        name: 'Maria Salvatore',
+        speciality: 'Neurologist'
+    },
+    {
+        name: 'Miyoshiro Takahata',
+        speciality: 'Oncology'
+    }
+];
+
+export default doctors;
