c26b040324ad930d5c883e5b077ad608e92ec41a
marcos medeiros <mvsmdrs@protonmail.com>
2020-04-17 17:41:57 -0300

    Fix user controller internal error on post

--- api-server/app/controllers/api/v1/users_controller.rb
+++ api-server/app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,6 @@ class Api::V1::UsersController < Api::V1::BaseController
     private
 
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:id, :name)
     end 
 end
--- api-server/config/routes.rb
+++ api-server/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :aircrafts, only: [:index]
-      resources :users, only: [:index, :create]
+      resources :users, only: [:index, :create, :destroy]
       resources :tests, only: [:index, :create, :destroy]
     end
   end
